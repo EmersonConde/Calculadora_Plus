bc4897a670f3882972b0bcb1f6d1d0d9a0895d1c
Émerson Conde <emersonsilvacaucaia27@gmail.com>
2024-10-30 11:34:42 +0000

    Versão 1.1

--- obj/Debug/net8.0/calculadora_plus.AssemblyInfo.cs
+++ obj/Debug/net8.0/calculadora_plus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculadora_plus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e439263eff4c0a6975c2f5e768bcb12ec80d7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculadora_plus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculadora_plus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
